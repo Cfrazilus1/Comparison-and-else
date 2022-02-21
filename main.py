849f5b1ca861233bf58b73fd64460c69915a97d2
Cfrazilus1 <99999698+Cfrazilus1@users.noreply.github.com>
2022-02-21 19:26:50 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+light_is_on = True
+
+if light_is_on:
+  print ("The light is on!")
+else:
+  print ("We're in the dark")
+
+weight = 329.8
+
+if weight > 300:
+  print ("You're overweight :(")
+
+age = 25
+
+if age >= 18:
+  print ("You're an adult!")
+
+else:
+  print ("You're a Child")
+  
